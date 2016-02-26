fb845971df1103975b742b2fcc6503bf8e6de6b9
Graham Lally <graham.lally@ocsi.co.uk>
2016-02-26 15:29:37 +0000

    HostConfig and HostConfigManager now support loading via access tokens

--- httpdocs/.includes/library/local/classes/HostConfig.php
+++ httpdocs/.includes/library/local/classes/HostConfig.php
@@ -18,13 +18,21 @@ class HostConfig {
     /**
      * Constructor. Requires a domain and a database connection.
      * 
-     * @param $host_domain  String  Domain to use for host - no http:// or trailing /
-     * @param $db_connection MySqlConnection MySQL Database Connection object to use  
+     * @param String $host_domain  String  Domain to use for host - no http:// or trailing /
+     * @param MySqlConnection $db_connection MySqlConnection MySQL Database Connection object to use  
+     * @param String $access_token Optional access token, which will force a check on token when loading config
      */    
-    public function __construct($host_domain, MySqlConnection $db_connection) {
+    public function __construct($host_domain, 
+                                MySqlConnection $db_connection,
+                                $access_token = null) {
         $this->domain = $host_domain;
         $this->db_connection = $db_connection;
-        $this->fetchHostDetails();
+        
+        if (is_null($access_token)) {
+            $this->fetchHostDetails();
+        } else {
+            $this->fetchHostDetailsUsingAccessToken($access_token);
+        }
     }
     
     public function fetchHostDetails() {
@@ -43,8 +51,50 @@ class HostConfig {
                             )
                         );
                         
-        $host_info = $host_info[0];
+        $this->setFromHostInfo($host_info[0]);
         
+    }
+    
+    /**
+     * Loads details using access token as double-check. 
+     * 
+     * @param String $access_token Token to use, cannot be null or empty
+     * 
+     * @throws Exception if access token is null or empty, or if couldn't load config 
+     */
+    public function fetchHostDetailsUsingAccessToken($access_token) {
+        
+        if (! $access_token) {
+            throw new Exception('fetchHostDetailsUsingAccessToken() must be called with a token.');
+        }
+        
+        $host_info = $this
+                        ->db_connection
+                        ->link
+                        ->select(
+                            self::DB_TABLE__HOSTS,
+                            array(
+                                'host_id',
+                                'host_token',
+                                'parent_host_id'
+                            ),
+                            array(
+                                'AND' => array(
+                                    'host_domain' => $this->getDomain(),
+                                    'host_token' => $access_token
+                                )
+                            )
+                        );
+        
+        if (count($host_info) == 0) {
+            throw new Exception('Unable to find host details, either host domain or access token is wrong?');
+        }   
+                     
+        $this->setFromHostInfo($host_info[0]);
+        
+    }
+    
+    private function setFromHostInfo($host_info) {
         $this->id = $host_info['host_id'];
         $this->token = $host_info['host_token'];
         $this->parent_host_id = $host_info['parent_host_id'];
--- httpdocs/.includes/library/local/classes/HostConfigManager.php
+++ httpdocs/.includes/library/local/classes/HostConfigManager.php
@@ -7,6 +7,7 @@
 class HostConfigManager {
     
     private $target_host_config_id = null;
+    private $target_host_config_access_token = null;
     private $target_host_config_domain = null;
     
     /* @var $db_connection MySqlConnection *.
@@ -17,9 +18,10 @@ class HostConfigManager {
      * ID or domain.
      * 
      * @param Mixed $id_or_domain Pass in an int to load by ID, or a non-int to load by domain.
+     * @param Mixed $access_token String to force security check when loading, or null to not check
      * @param MySqlConnection $db_connection Database connection to use
      */
-    public function __construct($id_or_domain, MySqlConnection $db_connection) {
+    public function __construct($id_or_domain, $access_token, MySqlConnection $db_connection) {
         
         if (is_int($id_or_domain)) {
             $this->target_host_config_id = (int) $id_or_domain;
@@ -29,10 +31,15 @@ class HostConfigManager {
             $this->target_host_config_domain = $id_or_domain;
         }
         
+        if (! is_null($access_token)) {
+            $access_token = trim($access_token);
+        }
+        $this->target_host_config_access_token = $access_token;
+        
         $this->db_connection = $db_connection;
         
     }
- 
+
     /**
      * Gets the HostConfig object for the given setup if we can.
      * 
@@ -47,12 +54,46 @@ class HostConfigManager {
         if (! is_null($this->target_host_config_domain)) {
             $host_config = new HostConfig(
                                           $this->target_host_config_domain, 
-                                          $this->db_connection
+                                          $this->db_connection,
+                                          $this->target_host_config_access_token
                                          );
             return $host_config;
         } else {
-            throw Exception('Sorry, loading HostConfigs by ID isn\'t supported yet.');
+            throw new Exception('Sorry, loading HostConfigs by ID isn\'t supported yet.');
         }
     }
-    
+ 
+     /**
+      * Gets the HostConfig object for the given setup if we can, but making sure we do
+      * security checks using access token.
+      * 
+      * Doesn't support ID loading yet, needs a quick translation to domain.
+      * 
+      * @return HostConfig HostConfig class
+      * 
+      * @throws Exception if trying to load by ID, no token set, or token invalid
+      */   
+    /*
+    public function getHostConfigWithToken() {
+        
+        if (is_null($this->target_host_config_domain)) {
+            throw new Exception('Sorry, loading HostConfigs by ID isn\'t supported yet.');
+        } else if (! $this->target_host_config_access_token) {
+            throw new Exception('Sorry, can\'t look a host up without an access token.');
+        }
+
+        try {        
+            $host_config = new HostConfig(
+                                          $this->target_host_config_domain, 
+                                          $this->db_connection,
+                                          $this->target_host_config_access_token
+                                         );
+        } catch (Exception $e) {
+            throw new Exception('Sorry, couldn\'t access host - invalid access token?');            
+        }
+        
+        return $host_config;
+                
+    }
+    */    
 }
