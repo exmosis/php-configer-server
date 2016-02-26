8be2d69df29739e743df72cbb10cdbae72e589d6
Graham Lally <graham.lally@ocsi.co.uk>
2016-02-26 15:43:43 +0000

    confirm_access now sends back json response

--- httpdocs/web_entries/confirm_access.php
+++ httpdocs/web_entries/confirm_access.php
@@ -5,10 +5,16 @@ $host = $http_request->getParameter('host');
 $token = $http_request->getParameter('token');
 
 $host_config_manager = new HostConfigManager($host, $token, $GLOBALS['php_configer_server_db_connection']);
-$host_config = $host_config_manager->getHostConfig();
 
-echo "<pre>";
-print_r($host_config);
-echo "</pre>";
+$response = new JsonResponse();
+
+try {
+    $host_config = $host_config_manager->getHostConfig();
+    $response->setSuccess(true);
+} catch (Exception $e) {
+    $response->addError($e->getMessage());
+}
+
+$response->go();
 
 
