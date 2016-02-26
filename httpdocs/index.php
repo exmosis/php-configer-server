82a4ce1bef004806e4d376ef886d0f4609cf1cef
Graham Lally <graham.lally@ocsi.co.uk>
2016-02-26 15:30:38 +0000

    Basic support for using web entry points based on URL

--- httpdocs/index.php
+++ httpdocs/index.php
@@ -2,6 +2,23 @@
 
 require_once('scripts/init.php');
 
+$script = $_GET['__configer_web_entry'];
+unset($_GET['__configer_web_entry']);
+
+try {
+    if (file_exists($GLOBALS['php_configer_server_web_entries_dir'])) {
+        require_once($GLOBALS['php_configer_server_web_entries_dir'] . $script . '.php');
+    } else {
+        echo 'ERROR 404'; // placeholder
+    }
+} catch (Exception $e) {
+    // Should setup an HtmlResponse here instead
+    echo '<h1>Error</h1>';
+    echo '<p>' . $e->getMessage() . '</p>';
+}
+
+exit;
+
 // echo 'Configer OK';
 
 echo "<p>Configer site OK. Getting setup for domain 'test'...</p>";
