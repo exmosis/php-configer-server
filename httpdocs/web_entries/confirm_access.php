dc29b5c6c3b9ea85eb967ca0261fec07c32b1145
Graham Lally <graham.lally@ocsi.co.uk>
2016-09-09 15:28:10 +0100

    Add ConfigVarManager DI when setting up HostConfigManager

--- httpdocs/web_entries/confirm_access.php
+++ httpdocs/web_entries/confirm_access.php
@@ -3,8 +3,9 @@
 $http_request = new HttpRequest(array('host', 'token'));
 $host = $http_request->getParameter('host');
 $token = $http_request->getParameter('token');
+$config_var_manager = new ConfigVarManager($GLOBALS['php_configer_server_db_connection']);
 
-$host_config_manager = new HostConfigManager($host, $token, $GLOBALS['php_configer_server_db_connection']);
+$host_config_manager = new HostConfigManager($host, $token, $GLOBALS['php_configer_server_db_connection'], $config_var_manager);
 
 $response = new JsonResponse();
 
